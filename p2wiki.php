54e14bc088f5a0f860f5e61968674766c67f0a88
Umherirrender <umherirrender_de.wp@web.de>
2021-10-24 16:20:39 +0200

    Emit deprecation warning from php entry point
    Change-Id: Iec233f5273cd3317655fa4eeaa6f1a396e7a31e5

--- p2wiki.php
+++ p2wiki.php
@@ -26,10 +26,10 @@ if ( function_exists( 'wfLoadSkin' ) ) {
 	wfLoadSkin( 'p2wiki' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['p2wiki'] = __DIR__ . '/i18n';
-	/*wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for p2wiki skin. Please use wfLoadSkin instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	);*/
+	);
 	return;
 } else {
 	die( 'This version of the p2wiki skin requires MediaWiki 1.25+' );
